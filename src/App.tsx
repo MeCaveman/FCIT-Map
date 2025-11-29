0bd790196ca6037ed30ec023b939e601b504cae5
MeCaveman <o.alghamdi97@gmail.com>
2025-11-29 21:13:22 +0300

    UI 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import { Suspense, createContext } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./styles/App.css";
 import Map from "./pages/Map";
+import Landing from "./pages/Landing";
 import { AdminContextType } from "./utils/types";
 import Loading from "./pages/Loading";
 import { Analytics } from "@vercel/analytics/react";
+
 export const AdminContext = createContext<AdminContextType | null>(null);
+
 function App() {
   return (
     <Suspense fallback={<Loading />}>
       <div className="flex flex-col relative w-full max-h-[100dvh]">
         <Routes>
-          <Route path="/:postion?" element={<Map />} />
+          {/* Default page */}
+          <Route path="/" element={<Landing />} />
+
+          {/* Map page */}
+          <Route path="/map/:postion?" element={<Map />} />
+
+          {/* Redirect unknown routes to landing */}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+
         <ToastContainer position="bottom-left" closeOnClick autoClose={2500} />
         <Analytics />
       </div>
@@ -23,3 +34,4 @@ function App() {
 }
 
 export default App;
+
