c06c052245356e99d6e7dbf93b5117b1f180daa8
MeCaveman <o.alghamdi97@gmail.com>
2025-11-29 21:05:05 +0300

    UI 6

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -11,7 +11,6 @@ import {
   NavigationContextType,
 } from "../utils/types";
 import Sidebar from "@/components/Sidebar";
-import Loading from "./Loading";
 
 export const NavigationContext = createContext<NavigationContextType | null>(
   null
@@ -35,21 +34,16 @@ function Map() {
 
   useEffect(() => {
     setSearchParams({ position: navigation.start });
-  }, [navigation.start, setSearchParams]);
+  }, [navigation.start]);
 
   const mapData = useMapData();
-  
-  if (mapData.isLoading) {
-    return <Loading />;
-  }
-
   return (
     <MapDataContext.Provider value={mapData}>
       <NavigationContext.Provider value={navigationValue}>
         <div className="flex bg-gray-100 text-gray-800 relative overflow-hidden w-full h-screen">
           {isDesktop && <Sidebar />}
           <main
-            className={`flex w-full ${isDesktop && "-ml-96"} justify-center flex-grow flex-col md:p-10 p-2 transition-all duration-150 ease-in lg:ml-0 relative`}
+            className={`flex w-full ${isDesktop && "-ml-96"} justify-center flex-grow flex-col md:p-10 p-2 transition-all duration-150 ease-in lg:ml-0`}
           >
             <Toolbar />
             <div className="center w-full h-full">
