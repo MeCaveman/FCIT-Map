4341dbf347a80147ddf51fdcf692d099c5238c4d
MeCaveman <o.alghamdi97@gmail.com>
2025-12-01 22:35:57 +0300

    FCIT-M 0.8.17

--- src/components/IndoorMap/MapBackground.tsx
+++ src/components/IndoorMap/MapBackground.tsx
@@ -42,7 +42,7 @@ function MapBackground({ children }: MapBackgroundProps) {
       key={currentFloor}
       viewBox={floorConfig.viewBox}
       preserveAspectRatio="xMidYMid meet"
-      className="lg:h-[85vh] lg:w-[75vw] h-[85dvh] w-auto"
+      className="w-full h-full"
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       onClick={handleSvgClick}
